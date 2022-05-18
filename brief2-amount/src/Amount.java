3b93d11b36418c7fc77b7b8cb865f848032c39bf
Najib-Rachid <kawner93@gmail.com>
2022-05-18 09:54:34 +0200

    fix prefrences

--- brief2-amount/src/Amount.java
+++ brief2-amount/src/Amount.java
@@ -5,12 +5,12 @@ public class Amount {
 	public static void main(String[] args) {
 
 		String name, paymentType;
-		double salary, 
-        hours, normalHours, maxHours, 
-        overTime, overTimePayPercent50,
-        hourPayRate, minHourPayRate;
+		double salary,
+				hours, normalHours, maxHours,
+				overTime, overTimePayPercent50,
+				hourPayRate, minHourPayRate;
 		char operationType;
-		Scanner reader = new Scanner(System.in); 
+		Scanner reader = new Scanner(System.in);
 
 		System.out.println("****** SalaryCalculator ******");
 		System.out.println("Enter your Name: ");
@@ -30,45 +30,51 @@ public class Amount {
 				System.out.println("Exit!");
 				reader.close();
 				break;
-			}
-            else if (operationType == '1') {
-                paymentType = "Weekly"; minHourPayRate = 25; normalHours = 40; maxHours= 50;
-            } 
-            else if(operationType == '2'){
-                paymentType = "Monthly"; minHourPayRate = 20; normalHours = 180; maxHours= 200;
-            }
-            else {
+			} else if (operationType == '1') {
+				paymentType = "Weekly";
+				minHourPayRate = 25;
+				normalHours = 40;
+				maxHours = 50;
+			} else if (operationType == '2') {
+				paymentType = "Monthly";
+				minHourPayRate = 20;
+				normalHours = 180;
+				maxHours = 200;
+			} else {
 				System.out.println("Error opertaion doesnt exist!");
-                continue;
+				continue;
 			}
 
-            while(true){
-		        System.out.printf("Enter the number of %s hours worked: ", paymentType);
+			while (true) {
+				System.out.printf("Enter the number of %s hours worked: ", paymentType);
 				hours = reader.nextDouble();
-                if (hours <= maxHours)
-                    break;
-                System.out.printf("The total number of %s hours must not exceed %s hours per week!\r\n", paymentType, maxHours);
-            }
+				if (hours <= maxHours)
+					break;
+				System.out.printf("The total number of %s hours must not exceed %s hours per week!\r\n", paymentType,
+						maxHours);
+			}
 
 			do {
-			    System.out.print("Enter Pay Rate Per Hour: ");
+				System.out.print("Enter Pay Rate Per Hour: ");
 				hourPayRate = reader.nextDouble();
 				if (hourPayRate < minHourPayRate)
-					System.out.printf("The %s pay rate per hour must not be less than %s DH per hour!\r\n", paymentType, minHourPayRate);
+					System.out.printf("The %s pay rate per hour must not be less than %s DH per hour!\r\n", paymentType,
+							minHourPayRate);
 			} while (hourPayRate < minHourPayRate);
 
 			salary = hours * hourPayRate;
 			if (normalHours < hours && hours <= maxHours) {
 				overTime = hours - normalHours;
-			    overTimePayPercent50 = overTime*hourPayRate/2;
-                System.out.printf("%s dh (normalPay) + %s dh (the 50%% for your %s overtime hours).", salary, overTimePayPercent50 ,overTime);
-				salary += overTime*hourPayRate/2;				
-		    }
+				overTimePayPercent50 = overTime * hourPayRate / 2;
+				System.out.printf("%s dh (normalPay) + %s dh (the 50%% for your %s overtime hours).", salary,
+						overTimePayPercent50, overTime);
+				salary += overTime * hourPayRate / 2;
+			}
 			System.out.printf("Your Total %s Salary is %s DH.", paymentType, salary);
 
-      } while (true);
+		} while (true);
 
-	  reader.close();
+		reader.close();
 
 	}
 

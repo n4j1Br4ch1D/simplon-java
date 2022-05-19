c811c5060138757395af2131b752cd0e9ac58cdc
Najib-Rachid <kawner93@gmail.com>
2022-05-19 15:06:36 +0200

    add operation conditing brief2 oop

--- brief2-oop-salaryCalculator/src/Main.java
+++ brief2-oop-salaryCalculator/src/Main.java
@@ -17,7 +17,18 @@ public class Main {
 			System.out.println("1-Employee Fixed.\n2-Employee Commission.\n3-Employee Hourly\n0-Exit.");
 			System.out.print("Enter type of the operation: ");
 			operationType = reader.next().charAt(0);
+			if (operationType == '0') {
+				System.out.println("Exit!");
+				reader.close();
+				break;
+			} else if (operationType == '1') {
+		
+			} else if (operationType == '2') {
 
+			} else {
+				System.out.println("Error opertaion doesnt exist!");
+				continue;
+			}
 		
             System.out.println("Enter Employee FullName: ");
 	     	String fullname = reader.nextLine();
@@ -38,65 +49,66 @@ public class Main {
 			// employeeHoursWorked = hoursWorked;
 			// employeeSalePrice = salePrice;
 
-		JavaDeveloper developerObj = new JavaDeveloper(name, age);
-		developerObj.setSalary(salary);
+//		 Employee employeeObj = new Employee();
+//		 employeeObj.setSalary(salary);
 		
 		}while(true);
 
+		reader.close();
 	}
 
-	class Employee {
+//	class Employee {
+//
+//		final static String DEPARTMENT = "Cosmetics";
+//		static public String firstName;
+//		static public String employeeLastName;
+//		static public int employeeBirthDate;
+//		static private double employeeSalary;
+//		static private int employeeSales;
+//		static private double employeeCommission;
+//		static private double employeeHoursWorked;
+//		static private int employeeSalePrice;
+//
+//		public Employee(String firstName, String lastName,  int birthDate,double salary, int sales, double commission, double hoursWorked, int salePrice ) {
+//			this.firstName = firstName;
+//			employeeLastName = lastName;
+//			employeeBirthDate = birthDate;
+//			employeeSalary = salary;
+//			employeeSales = sales;
+//			employeeCommission = commission;
+//			employeeHoursWorked = hoursWorked;
+//			employeeSalePrice = salePrice;
+//		}
+//
+//		public static double getSalary() {
+//			return employeeSalary;
+//		}
+//
+//		public void setSalary(double salary) {
+//			employeeSalary = salary;
+//		}
+//
+//	}
+//
+//	class EmployeeFixed extends Employee {
+//
+//		public EmployeeFixed(String name, int age) {
+//			super(name, age);
+//		}
+//
+//	}
 
-		final static String DEPARTMENT = "Cosmetics";
-		static public String firstName;
-		static public String employeeLastName;
-		static public int employeeBirthDate;
-		static private double employeeSalary;
-		static private int employeeSales;
-		static private double employeeCommission;
-		static private double employeeHoursWorked;
-		static private int employeeSalePrice;
+//	class EmployeeCommission extends Employee {
+//
+//		public EmployeeCommission(String name, int age) {
+//			super(name, age);
+//		}
+//
+//	}
 
-		public Employee(String firstName, String lastName,  int birthDate,double salary, int sales, double commission, double hoursWorked, int salePrice ) {
-			this.firstName = firstName;
-			employeeLastName = lastName;
-			employeeBirthDate = birthDate;
-			employeeSalary = salary;
-			employeeSales = sales;
-			employeeCommission = commission;
-			employeeHoursWorked = hoursWorked;
-			employeeSalePrice = salePrice;
-		}
-
-		public static double getSalary() {
-			return employeeSalary;
-		}
-
-		public void setSalary(double salary) {
-			employeeSalary = salary;
-		}
-
-	}
-
-	class EmployeeFixed extends Employee {
-
-		public EmployeeFixed(String name, int age) {
-			super(name, age);
-		}
-
-	}
-
-	class EmployeeCommission extends Employee {
-
-		public EmployeeCommission(String name, int age) {
-			super(name, age);
-		}
-
-	}
-
-class EmployeeHourly extends Employee {
-
-    public EmployeeHourly(String name, int age) {
-		super(name, age);
-	}
+//class EmployeeHourly extends Employee {
+//
+//    public EmployeeHourly(String name, int age) {
+//		super(name, age);
+//	}
 }

4d9b9b4e52717f30480d9622e8e787e28a55e835
Najib-Rachid <kawner93@gmail.com>
2022-05-20 12:50:42 +0200

    add brief 2 oop buissness logic

--- brief2-oop-salaryCalculator/src/Employee.java
+++ brief2-oop-salaryCalculator/src/Employee.java
@@ -1,65 +1,138 @@
+import java.util.Scanner;
 
 public class Employee {
-	final static String DEPARTMENT = "Cosmetics";
+	final static String DEPARTMENT = "Softwares";
 	public String firstName;
-	static public String employeeLastName;
-	static public String employeeBirthDate; // will do Date later
-	static private double employeeSalary;
-//	static private int employeeSales;
-//	static private double employeeCommission;
-//	static private double employeeHoursWorked;
-//	static private int employeeSalePrice;
+	public String employeeLastName;
+	static String employeeBirthDate; // will do Date later //min birthdate
+	static private int EmployeeAge; // will do Date later //min birthdate
+	static private int EmployeeMinHireAge = 16;
+	static private int employeePaymentDayDuration = 30; 
+	static protected double employeeSalary = 500;
+	Scanner reader = new Scanner(System.in);
 
-	public Employee(String firstName, String lastName, String dateInString ) { //double salary, int sales, double commission, double hoursWorked, int salePrice
+	public Employee(String firstName, String lastName, String dateInString ) {
 		this.firstName = firstName;
 		employeeLastName = lastName;
 		employeeBirthDate = dateInString;
-//		employeeSalary = salary;
-//		employeeSales = sales;
-//		employeeCommission = commission;
-//		employeeHoursWorked = hoursWorked;
-//		employeeSalePrice = salePrice;
 	}
 
-	public static double getSalary() {
+	public void setPaymentDayDuration(double salary) {
+		employeeSalary = salary;
+	}
+
+	public static double getPaymentDayDuration() {
 		return employeeSalary;
 	}
 
 	public void setSalary(double salary) {
 		employeeSalary = salary;
 	}
+
+	public static double getSalary() {
+		return employeeSalary;
+	}
+	
+	public double calculateSalary() {
+		return 343; //employeePaymentDayDuration*employeeSalary;
+	}
+
+	void askEmloyeeFullName() {
+	    System.out.println("Enter Employee FullName(firstName lastName): ");
+	    String hhh = "najib rachid";
+        String[] fullName = hhh.split("\\s+");  //reader.next()
+		firstName = fullName[0];
+		employeeLastName =   fullName[1];
+	}
+	
+	String askEmloyeeBirthDate() {
+		
+	 	System.out.println("Enter Employee BirthDate(D/M/Y): ");
+	    return reader.next();
+		
+//		do {
+//			
+//		 	System.out.println("Enter Employee BirthDate(D/M/Y): ");
+//			
+//		 	//EmployeeAge = Date.now() - employeeBirthDate.getYear();
+//		 	int EmployeeAge = 14;
+//			if (EmployeeAge < EmployeeMinHireAge)
+//				System.out.printf("We only Hire People With minimum age of %s ", EmployeeMinHireAge);
+//		} while (EmployeeAge < EmployeeMinHireAge);
+	
+		//int birthDate = reader.nextInt();
+	}
+
+	void info() {
+		System.out.println("Full Name: "+firstName+" "+employeeLastName+".");
+		System.out.println("Birth Date: "+DEPARTMENT+" Department.");
+		System.out.println("Department: "+DEPARTMENT+" .");
+	    System.out.println("Monthly Salary: "+calculateSalary()+".");
+	}
 }
 
 
 class EmployeeFixed extends Employee {
 
-	   public EmployeeFixed(String firstName,String lastName, String dateInString) {
+	public EmployeeFixed(String firstName,String lastName, String dateInString) {
 			super(firstName, lastName, dateInString);
-		}
-
-	public void salary() {
-		// TODO Auto-generated method stub
-		
 	}
 
-	public void info() {
-		// TODO Auto-generated method stub
-		System.out.print(123);
-		
-	}
 }
 
 class EmployeeCommission extends Employee {
 
-	   public EmployeeCommission(String firstName,String lastName, String birthDate) {
+	static private int employeeSalesNumber;
+	static private double employeeSalePrice = 100;
+	static private double employeeSaleCommisionPercentage = 50;
+	static private double employeeCommission = (employeeSalesNumber*employeeSalePrice)*employeeSaleCommisionPercentage/100;
+
+	double askEmployeeSalesNumber() {
+			System.out.println("Enter Employee Sales Number: ");
+		   return reader.nextDouble();
+	}
+								
+
+	public EmployeeCommission(String firstName,String lastName, String birthDate) {
 			super(firstName, lastName, birthDate);
-		}
+	}
+
+	public static double calculateSalary() {
+//		if(employeeSalesNumber > 100) {
+//			return employeeSaleCommisionPercentage = 80;
+//		}
+		//return employeeSalary + employeeCommission;
+		return 42; //employeeCommission;
+	}
 
 }
 
-class EmployeeHourly extends Employee {
+class EmployeeHourly extends Employee {   
 
-public EmployeeHourly(String firstName,String lastName, String birthDate) {
-	super(firstName, lastName, birthDate);
+	static private double employeeHoursWorked;
+	static private double employeeHoursPayRate = 100;
+
+	public EmployeeHourly(String firstName,String lastName, String birthDate) {
+			super(firstName, lastName, birthDate);
+	}
+
+	private void askEmployeeHoursWorked() {
+		System.out.println("Enter Employee Hours Worked: ");
+	   // double sales = reader.nextDouble();
+   }
+
+	public static double calculateSalary() {
+
+//		salary = hours * hourPayRate;
+//				overTime = hours - normalHours;
+//				overTimePayPercent50 = overTime * hourPayRate / 2;
+//				salary += overTime * hourPayRate / 2;
+		
+		return employeeHoursPayRate * employeeHoursWorked;
+	}
 }
-}
+
+//make it work;
+//improve features.
+//check oop.
+//make all oop.
--- brief2-oop-salaryCalculator/src/Main.java
+++ brief2-oop-salaryCalculator/src/Main.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 
 public class Main {
 
-	
     public static DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
     public static Date dateParser(String dateStr) {
         try {
@@ -18,70 +17,109 @@ public class Main {
 		return null;
     }
     
-	private static EmployeeFixed CEO = new EmployeeFixed("Najib", "Rachid", "7-07-1996");
-	private static EmployeeFixed COO = new EmployeeFixed("Omar", "Dbaa", "7-08-1996");
+	private static EmployeeFixed CEO = new EmployeeFixed("Najib", "Rachid", "7-07-1996");  //special 
+	private static EmployeeFixed COO = new EmployeeFixed("Omar", "Dbaa", "7-08-1996");  //special
 
 	public static void main(String[] args) {
 
 		char operationType;
 		Scanner reader = new Scanner(System.in);
-        
-     
 
 		System.out.format("+-----------------+-----------------+%n");
 		System.out.format("|           SalaryCalcultor         |%n");
-		System.out.format("|      NajibRachid &  OmarDbaa      |%n");
+		System.out.format("|       NajibRachid & OmarDbaa      |%n");
+		System.out.format("|                V0.0.1             |%n");
 		System.out.format("+-----------------+-----------------+%n");
 
 		do {
-			System.out.println("\n\nChoose Type of Employee:");
-			System.out.println("1-Employee Fixed.\n2-Employee Commission.\n3-Employee Hourly.\n4-CEO`s Salary.\n5-COO`s Salary.\n0-Exit.");
+			System.out.println("\n\nChoose Type of Operation:");
+			System.out.println("1-Employee Fixed.\n2-Employee Commission.\n3-Employee Hourly.\n4-CEO`s Salary.\n5-COO`s Salary.\n9-settings\n0-Exit!");
 			System.out.print("Enter type of the operation: ");
 			operationType = reader.next().charAt(0);
 			if (operationType == '0') {
 				System.out.println("Exit!");
 				reader.close();
 				break;
-			} else if (operationType == '1') {
-				
-		
-			} else if (operationType == '2') {
+			}else if(operationType == '1' || operationType == '2' || operationType == '3'){ //Employee Type;
+				  Employee employee = new Employee(null, null, null);
+				  employee.askEmloyeeFullName();
+				 // Employee.employeeBirthDate =  employee.askEmloyeeBirthDate();
 
-			} else if (operationType == '3') {
-				
+				 if (operationType == '1') {
+						System.out.println("Employee Fixed\n");
+
+					  EmployeeFixed employeeFix = new EmployeeFixed(null, null, null);
+					  
+						System.out.println("hhh"+employeeFix.calculateSalary());
+
+
+			     } else if (operationType == '2') {
+						System.out.println("Employee Commission\n");
+						
+
+				   EmployeeCommission employeeCom = new EmployeeCommission(null, null, null);
+				   employeeCom.askEmployeeSalesNumber();
+				   employeeCom.info();
+
+				   
+                 } else {
+				  System.out.println("Employee Hourly\n");
+  				   EmployeeHourly employeeHour = new EmployeeHourly(null, null, null);
+  				     employeeHour.info();
+
+                 }
+				   
+
 			} else if (operationType == '4') {
 				CEO.info();				
 		    } else if (operationType == '5') {
-				COO.info();				    
-		    }  else {
+			    COO.info();	// tari9a m5talfa			    
+			} else if (operationType == '9') {
+				System.out.println("\n\n##### Settings #####\n");
+				System.out.println("Choose Type of Operation:");
+				System.out.printf("1-set Salary:........................%s\n", 9000);
+				System.out.printf("2-set Sale Price:...................%s\n", 100);
+				System.out.printf("3-Set Sale Commision Percentage:            %s%%\n", 10);
+				System.out.printf("3-set Hours Pay Rate:           %s\n", 100);
+				operationType = reader.next().charAt(0);
+				//
+				//currency;
+				//EmployeeMinHireAge
+				
+				//employeeSalesNumberSpecial
+				//employeeSaleCommisionPercentageSpecial 
+
+	
+				//employeeHoursWorkedSpecial
+				//employeeHoursPayRateSpecial
+
+				System.out.println("9-Return.\n0-Exit!");
+
+				/* 
+   
+			
+	    	System.out.println("Enter Employee Salary: ");
+		    double salary = reader.nextDouble();
+		    employeeObj.setSalary(salary);
+
+			System.out.println("Enter Employee Sales price: ");
+		    double commission = reader.nextDouble();
+
+			System.out.println("Enter Employee Sales commission percentage: ");
+		    double commission = reader.nextDouble();
+
+
+			System.out.println("Enter Hours Pay Rate: ");
+		    double hoursPayRate = reader.nextDouble();
+			
+
+
+
+				*/
+		    } else {
 				System.out.println("Error opertaion doesnt exist!");
 				continue;
 			}
-		
-            System.out.println("Enter Employee FullName(firstName lastName): ");
-	     	String fullname = reader.next();
-	     	 
-	
-	     	
-		    System.out.println("Enter Employee BirthDate(D/M/Y): ");
-		    int birthDate = reader.nextInt();
-			
-			//2
-	    	System.out.println("Enter Employee Salary: ");
-		    double salary = reader.nextDouble();
-
-			System.out.println("Enter Employee Sales Number: ");
-		    double sales = reader.nextDouble();
-			
-			System.out.println("Enter Employee Sales commission: ");
-		    double commission = reader.nextDouble();
-			
-		
-			// employeeHoursWorked = hoursWorked;
-			// employeeSalePrice = salePrice;
-
-//		 Employee employeeObj = new Employee();
-//		 employeeObj.setSalary(salary);
 		
 		}while(true);
 

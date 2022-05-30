f1f2b3049285634796d8aa91c2fa14f1230927cc
Najib Rachid <38995898+n4j1Br4ch1D@users.noreply.github.com>
2022-05-30 14:09:48 +0200

    add migration delete set null to brief4 db

--- brief4-database/ahanou_online_migration.sql
+++ brief4-database/ahanou_online_migration.sql
@@ -1,17 +1,18 @@
+
 /*Create Table Users:*/
 CREATE TABLE IF NOT EXISTS public.users
 (
- id serial NOT NULL,
- email char(45) NOT NULL,
- password char(45) NOT NULL,
- first_name char(25) NOT NULL,
- last_name char(25) NOT NULL,
- approved boolean NOT NULL DEFAULT false,
- created_at timestamp with time zone NOT NULL DEFAULT NOW(),
- updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
- CONSTRAINT "Users_pkey" PRIMARY KEY (id, email),
- UNIQUE (id),
- UNIQUE (email) 
+id serial NOT NULL,
+email char(45) NOT NULL,
+password char(45) NOT NULL,
+first_name char(25) NOT NULL,
+last_name char(25) NOT NULL,
+approved boolean NOT NULL DEFAULT false,
+created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
+CONSTRAINT "Users_pkey" PRIMARY KEY (id, email),
+UNIQUE (id),
+UNIQUE (email) 
 );
 
 /*Alter Table Users Add Role Column:*/
@@ -21,14 +22,14 @@ ALTER TABLE public.users Alter COLUMN "role_type" SET DEFAULT 'client';
 /*Create Table Categories:*/
 CREATE TABLE IF NOT EXISTS public.categories
 (
-  id serial NOT NULL,
-  name char(45) NOT NULL,
-  img char(255),
-  created_at timestamp with time zone NOT NULL DEFAULT NOW(),
-  updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
-  CONSTRAINT "categories_pkey" PRIMARY KEY (id, name),
-  UNIQUE (id),
-  UNIQUE (name) 
+id serial NOT NULL,
+name char(45) NOT NULL,
+img char(255),
+created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
+CONSTRAINT "categories_pkey" PRIMARY KEY (id, name),
+UNIQUE (id),
+UNIQUE (name) 
 );
 
 /*Create Table Products:*/
@@ -45,37 +46,38 @@ category_id int,
 created_at timestamp with time zone NOT NULL DEFAULT NOW(),
 updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
 CONSTRAINT "products_pkey" PRIMARY KEY (id, name),
-   UNIQUE (id),
-   UNIQUE (name),
+ UNIQUE (id),
+ UNIQUE (name),
 CONSTRAINT "category_fkey"
-  FOREIGN KEY(category_id) 
-  REFERENCES public.categories(id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID
+FOREIGN KEY(category_id) 
+REFERENCES public.categories(id) MATCH SIMPLE
+  ON UPDATE NO ACTION
+  ON DELETE SET NULL
+  NOT VALID
 );
 
 /*Create Table Votes:*/ 
 CREATE TABLE IF NOT EXISTS public.votes
 (
-  id serial NOT NULL,
-  vote_percent int NOT NULL,
-  client_id int NOT NULL,
-  product_id int NOT NULL,
-  created_at timestamp with time zone NOT NULL DEFAULT NOW(),
-  updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
-  CONSTRAINT "votes_pkey" PRIMARY KEY (id),
-  UNIQUE (id),
-  CONSTRAINT "client_fkey"
-    FOREIGN KEY(client_id) 
-    REFERENCES public.users(id) MATCH SIMPLE
-      ON UPDATE NO ACTION
-      ON DELETE NO ACTION
-      NOT VALID,
-   CONSTRAINT "product_fkey"
-    FOREIGN KEY(product_id) 
-    REFERENCES public.products(id) MATCH SIMPLE
-      ON UPDATE NO ACTION
-      ON DELETE NO ACTION
-      NOT VALID
+id serial NOT NULL,
+vote_percent int NOT NULL,
+client_id int NOT NULL,
+product_id int NOT NULL,
+created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
+CONSTRAINT "votes_pkey" PRIMARY KEY (id),
+UNIQUE (id),
+CONSTRAINT "client_fkey"
+  FOREIGN KEY(client_id) 
+  REFERENCES public.users(id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE
+    NOT VALID,
+ CONSTRAINT "product_fkey"
+  FOREIGN KEY(product_id) 
+  REFERENCES public.products(id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE
+    NOT VALID
 );
+  

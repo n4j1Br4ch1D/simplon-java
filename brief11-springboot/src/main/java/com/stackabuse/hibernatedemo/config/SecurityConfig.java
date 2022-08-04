35664f632886f664623a298defdbf16c6f700795
Najib-Rachid <kawner93@gmail.com>
2022-08-04 19:36:09 +0200

    fix security no password brief 11

--- brief11-springboot/src/main/java/com/stackabuse/hibernatedemo/config/SecurityConfig.java
+++ brief11-springboot/src/main/java/com/stackabuse/hibernatedemo/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.stackabuse.hibernatedemo.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -26,12 +28,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	System.out.println(111123);
         auth.jdbcAuthentication().dataSource(dataSource)
-                .usersByUsernameQuery("select email as principal, password as credentails, true from member where email=?")
-                .authoritiesByUsernameQuery("select email as principal, role as role from role where email=?");
-//                .passwordEncoder(passwordEncoder()).rolePrefix("ROLE_");
+                .usersByUsernameQuery("select email as principal, password as credentails, true from users where email=?")
+                .authoritiesByUsernameQuery("select email as principal, role as role from users where email=?");
+//                .passwordEncoder(passwordEncoder());
     }
   
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
   
   
  
--- brief11-springboot/src/main/java/com/stackabuse/hibernatedemo/controller/AuthController.java
+++ brief11-springboot/src/main/java/com/stackabuse/hibernatedemo/controller/AuthController.java
@@ -1,15 +1,23 @@
 package com.stackabuse.hibernatedemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 	public class AuthController {
 
 	    @GetMapping("/signin")
-	    public String showLoginForm(){
+	    public String showLoginForm(Model model) throws Exception {
+//			model.addAttribute("user", userService.getUserById(userId));
+//			model.addAttribute("type", "Read");
+//			model.addAttribute("SigninError", error);
+			model.addAttribute("email", "leila@anmoon.ma");
+			model.addAttribute("password", "password");
 	        return "signin";
 	    }
+	    
 
 	}
 

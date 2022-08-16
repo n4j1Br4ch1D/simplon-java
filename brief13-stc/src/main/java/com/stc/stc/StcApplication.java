8102d932957e340bea70578573db68ed10c6cffb
Najib-Rachid <kawner93@gmail.com>
2022-08-16 13:01:18 +0200

    addd brief 13

---
+++ brief13-stc/src/main/java/com/stc/stc/StcApplication.java
@@ -0,0 +1,13 @@
+package com.stc.stc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StcApplication.class, args);
+	}
+
+}
---
+++ brief13-stc/src/test/java/com/stc/stc/StcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stc.stc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

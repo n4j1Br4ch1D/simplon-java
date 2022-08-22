5d66df34ae11c3deb47edb21a9c4cbb6f31b7c76
idmed22 <idlmhor.med@gmail.com>
2022-08-22 12:07:05 +0100

    add brief13 init seeders

--- brief13-stc/src/main/java/com/stc/stc/db/users_seeder.sql
+++ brief13-stc/src/main/java/com/stc/stc/db/users_seeder.sql
@@ -1,10 +1,71 @@
 /*Seed Table Users:*/
  INSERT INTO public.users 
-   (full_name, role, approved, email, password, tasks_number)
- VALUES
-   ('Leila M`haidrat', 'admin', true, 'leila@anmoon.ma', 'password', null),
-   ('Najib Rachid', 'employee', true, 'najib@anmoon.ma', 'password', 0),
-   ('Anas Oussous', 'employee', true, 'najib@anmoon.ma', 'password', 1),
-   ('Fatiha El habti', 'employee', false, 'anas@anmoon.ma', 'password', 2),
-   ('Seny Balde', 'employee', false, 'seny@anmoon.ma', 'password', 3),
-   ('Ayoub Oudor', 'employee', false, 'ayoub@anmoon.ma', 'password', 4);
+   (full_name, email, password, tel, role, enabled,  status)
+     VALUES
+         ('Najib Rachid','najib@anmoon.ma','password', '06000000','admin',true  ,null),
+         ('Mohamed id ','med@anmoon.ma','password', '06000040','manager',true ,null),
+         ('Ayoub Oudor ','ayoub@anmoon.ma','password', '06000040','manager',true ,null),
+         ('Fatiha El habti','anas@anmoon.ma','password', '06000090','participant',true ,null),
+         ('Seny Balde','seny@anmoon.ma','password', '06000090','participant',true ,null),
+         ('Anass EL','anas@anmoon.ma','password', '06000090','participant',true ,null),
+         ('Omar db','omar@anmoon.ma','password', '06040090','participant', false  ,null);
+
+
+   
+
+INSERT INTO public.admins
+    (id, level, enabled)
+    VALUES
+        (1, 'senior', true);
+
+           
+INSERT INTO public.managers
+    (id, domain, type, enabled)
+    VALUES
+         (2, 'Backend','Database', true),
+         (3, 'Backend','Spring boot', true);
+
+
+                     
+INSERT INTO public.participants
+    (id ,domain ,isMale ,enabled)
+    VALUES
+           (4, 'Backend', false, true),
+           (5, 'Backend', true, true),
+           (6, 'Backend', true, false);
+
+
+
+
+INSERT INTO public.activities
+        (id, name, description, type, status, started_at, ended_at, manager_id, exercises_id) 
+        VALUES
+            (1,  'Java' , 'Java is ...' , 'Backend', 'inprogrees', 22-08-22 , null,  2, 1),
+            (2,  'Angular' , 'Angular is ...' , 'Frontend','inprogrees' , 22-08-22 , null, 3, 1),
+            (3,  'PostgreSQL' , 'PostgreSQL is ...' , 'Database','ended',22-08-22 , null, 2, 3),
+            (4,  'Bootstrap' , 'Bootstrap is ...' , 'css framework','inprogrees' , 22-08-22 , null, 3, 4);      
+
+
+
+
+INSERT INTO public.exercises 
+      (id, name, startedAt, endedAt , activity_id)
+        VALUES
+         (1,  'Exercise1', 22-08-22, 30-08-22, 1),
+         (2,  'Exercise2', 22-08-22, 12-08-22, 3),
+         (3,  'Exercise3', 22-08-22, 03-08-22, 3),
+         (4,  'Exercise4', 22-08-22, 27-08-22, 4);
+        
+
+
+INSERT INTO public.activities_participants
+      (activity_id, participant_id)
+        VALUES
+          (1, 4),
+          (1, 5),
+          (1, 6),
+          (2, 4),
+          (3, 5);
+
+
+
